8a0570dd437ac21dc9c6a8f0cc6fcbea925ece04
Halifax96 <esalap00@gmail.com>
2023-02-22 06:29:55 -0800

    firstcommitaso

---
+++ fork.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, const char *argv[]){
+
+	
+	switch(fork()){
+	
+	case -1:
+		printf("Error al crear los procesos\n");
+		exit(1);
+	case 0:
+		printf("Hola soy el hijo\n");
+
+	default:
+		sleep(1);
+		printf("Hola, soy el padre\n");
+
+	}
+
+
+
+	return 0;
+}
